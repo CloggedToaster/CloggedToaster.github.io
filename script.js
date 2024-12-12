5f2a7fd41e85bec998fc3e8794e3aa05684ffdef
Clogged Toaster <jjavier848@outlook.com>
2024-12-12 01:22:51 -0500

    Changing JS onload

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-let count = 0;
+window.onload = function() {
+    let count = 0;
 
 
-const numberDisplay = document.getElementById('numberDisplay');
-const minusButton = document.getElementById('minusButton');
-const plusButton = document.getElementById('plusButton');
+    const numberDisplay = document.getElementById('numberDisplay');
+    const minusButton = document.getElementById('minusButton');
+    const plusButton = document.getElementById('plusButton');
 
-const updateDisplay = () => {
-    numberDisplay.textContent = count;
+    const updateDisplay = () => {
+        numberDisplay.textContent = count;
 
-    if(count === 0) {
-        minusButton.style.display = 'none';
-    } else {
-        minusButton.style.display = 'flex';
+        if(count === 0) {
+            minusButton.style.display = 'none';
+        } else {
+            minusButton.style.display = 'flex';
+        }
+
+    }
+    updateDisplay();
+
+
+    const handlePlusButtonClick = () => {
+        count++;
+        console.log(count);
+        updateDisplay();
     }
 
-}
-updateDisplay();
+    const handleMinusButtonClick = () => {
+        count--;
+        console.log(count);
+        updateDisplay();
+    }
 
-
-const handlePlusButtonClick = () => {
-    count++;
-    console.log(count);
-    updateDisplay();
-}
-
-const handleMinusButtonClick = () => {
-    count--;
-    console.log(count);
-    updateDisplay();
-}
-
-const updateCountText = () => {
-    mainNumber.innerHTML = count;
-}
-  
-document.getElementById('plusButton').addEventListener('click', handlePlusButtonClick);
-document.getElementById('minusButton').addEventListener('click', handleMinusButtonClick);
-  
+    const updateCountText = () => {
+        mainNumber.innerHTML = count;
+    }
+    
+    document.getElementById('plusButton').addEventListener('click', handlePlusButtonClick);
+    document.getElementById('minusButton').addEventListener('click', handleMinusButtonClick);
+};
